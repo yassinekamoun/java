762be619765a0dc6d0d839130c8e862e45826307
yassinekamoun <132911111+yassinekamoun@users.noreply.github.com>
2024-10-01 13:13:53 +0100

    Add files via upload

--- java/src/Zoo.java
+++ java/src/Zoo.java
@@ -3,17 +3,79 @@ public class Zoo {
     Animal [] animals ;
     String name ;
     String city ;
-    int nbrCages ;
+    final int nbrCages = 25 ;
+    int count;
 
 
-    public Zoo(String name, String city, int nbrCages)
+    public Zoo(String name, String city,  int count)
     {
-        this.animals = new Animal[25];
+        this.animals = new Animal[nbrCages];
         this.name = name ;
         this.city = city ;
-        this.nbrCages = nbrCages;
+        this.count = 0 ;
     }
     public String toString(){
-        return "Zoo Name :" + name + " \nCity: " + city + " \nNbrCages: " + nbrCages;
+        return "Zoo Name :" + name + " \nCity: " + city ;
     }
+
+    public boolean addAnimal(Animal animal) {
+        // Vérifier si l'animal existe déjà dans le zoo
+        if (searchAnimal(animal) != -1) {
+            System.out.println("L'animal existe déjà dans le zoo.");
+            return false;  // Ne pas ajouter l'animal s'il existe déjà
+        }
+
+        // Vérifier si le zoo a atteint sa capacité maximale
+        if (count >= animals.length) {
+            System.out.println("Le zoo est plein, impossible d'ajouter l'animal.");
+            return false;  // Le zoo est plein
+        }
+
+        // Ajouter l'animal et incrémenter le compteur
+        animals[count] = animal;
+        count++;
+        return true;  // L'animal a été ajouté avec succès
+    }
+
+    int searchAnimal(Animal animal) {
+        for (int i = 0; i < count ; i++) {
+            if (animals[i].name.equals(animal.name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public boolean removeAnimal(Animal animal)
+    {
+        int index = searchAnimal(animal);
+        if (index == -1) {
+            System.out.println("L'animal n'a pas été trouvé dans le zoo.");
+            return false;
+        }
+
+        for (int i = index; i < count - 1; i++) {
+            animals[i] = animals[i + 1];
+        }
+
+        animals[count - 1] = null;
+        count--;
+        System.out.println("L'animal a été supprimé du zoo avec succès.");
+        return true;
+    }
+    public boolean isZooFull() {
+        return count >= nbrCages;
+    }
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.count > z2.count) {
+            return z1;
+        } else if (z2.count > z1.count) {
+            return z2;
+        } else {
+            System.out.println("Les deux zoos contiennent le même nombre d'animaux.");
+            return null; // ou vous pouvez retourner l'un des zoos arbitrairement
+        }
+    }
+
+
+
 }
--- java/src/ZooManagement.java
+++ java/src/ZooManagement.java
@@ -1,14 +1,39 @@
-class Zoomanagement
+class ZooManagement
 {
 
     public static void main(String[] args)
     {
 
         Animal lion = new Animal("lion", "simba", 5, true);
-        Zoo myZoo = new Zoo("my zoo", "Ariana", 25);
+        Zoo myZoo = new Zoo("my zoo", "Ariana", 0);
+        Zoo z1 = new Zoo("belvedre","Tunis ", 0);
+
         System.out.println(myZoo);
         //System.out.println(myZoo.toString());//
+
+        Animal rlion=new Animal("Lion","simba" , 5 ,  true);
+        Animal tiger =new Animal("Tigre" ,"rose", 7, true);
+        Animal elephant =new Animal("Elephant" , "babar", 8, true );
+        Animal giraffe =new Animal("Giraffe", "ggg",9,true);
+        Animal doggy =new Animal("doggy", "doggy",10,true);
+        Animal chat =new Animal("chat", "chat",10,true);
+
+
+        System.out.println("Ajout du lion :" + myZoo.addAnimal(rlion));
+        System.out.println("Ajout du tigre :" + myZoo.addAnimal(tiger));
+        System.out.println("Ajout du elephant :" + myZoo.addAnimal(elephant));
+        System.out.println("Ajout du giraffe :" + myZoo.addAnimal(giraffe));
+        System.out.println("Ajout du doggy :" + myZoo.addAnimal(doggy));
+        System.out.println("Ajout du doggy :" + myZoo.addAnimal(doggy));
+
+        int index= myZoo.searchAnimal( doggy );
+        myZoo.removeAnimal( chat );
+
+        //System.out.println( index );//
+
     }
+
+
 
 }
 
